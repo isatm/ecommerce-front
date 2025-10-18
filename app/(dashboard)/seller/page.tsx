753eb85c8dfc69510db881905d60dcd2c8503904
Isabelalpal <isabela.silval@autonoma.edu.co>
2025-10-17 23:13:01 -0500

    Se corrigio para vercel

---
+++ app/(dashboard)/seller/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: "Seller page"
+};
+
+export default function sellerDashboardPage() {
+    return (
+        <div>
+            <h1>Seller Dashboard</h1>
+        </div>
+    );
+}
--- components/molecules/productItem.tsx
+++ components/molecules/productItem.tsx
@@ -4,14 +4,14 @@ import { userCartStore } from "@/hooks/usecartStore";
 import { useProductItem } from "@/hooks/useProductItem";
 
 export function ProductItem({ product }: Props) {
-    const {  addToCart } = useProductItem();
+    const {  handleAddToCart } = useProductItem();
 
     return (
         <div className="border p-4 rounded-lg shadow-md">
         <h2 className="text-lg font-bold">{product.name}</h2>
         <p>${product.price.toFixed(2)}</p>
         <button
-            onClick={() => addToCart(product)}
+            onClick={() => handleAddToCart(product)}
             className="bg-blue-500 text-white px-4 py-2 mt-2 rounded"
         >
             Añadir al carrito
