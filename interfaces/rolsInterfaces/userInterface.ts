83ff68a11070cb78ace5cfbf1c48a1de1f59fd63
Isabelalpal <isabela.silvalondo@gmail.com>
2025-10-28 02:41:17 -0500

    se hizo pagina de compras, pero no funciona ya que el cliente no tiene productos

--- interfaces/rolsInterfaces/userInterface.ts
+++ interfaces/rolsInterfaces/userInterface.ts
@@ -1,9 +1,13 @@
+import { Product } from "../shoppingInterfaces/productInterface"
+
 export interface UserDTO {
-  name: string
-  user: string
-  address: string
-  phone: string
-  password: string
+  name: string;
+  user: string;
+  address: string;
+  phone: string;
+  password: string;
+  products: Product;
+
 }
 
 export interface UserDAO extends UserDTO {
