e3d2ca9595e9f00c37847fcdae38ef08c1541d22
isatm <isabelataborda2005@gmail.com>
2025-10-29 13:25:12 -0500

    dominio de supabase para imagenes

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    domains: ['zbwfbkhommplpihdwrxa.supabase.co'],
     remotePatterns: [
       { protocol: "https", hostname: "solidsound.wpenginepowered.com"},
       { protocol: "https", hostname: "link-a-una-imagen.com" },
